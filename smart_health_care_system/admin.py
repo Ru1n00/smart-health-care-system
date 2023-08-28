5053db47f441a1e51a57b02c84e8412ee5e7a63a
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-28 11:41:48 +0600

    Register the models in admin panel

--- smart_health_care_system/admin.py
+++ smart_health_care_system/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doctor, BloodDonor, Ambulance
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'speciality', 'degrees_earned', 'position', 'location', 'visiting_place', 'visiting_hours', 'contact_info', 'img')
+
+
+class BloodDonorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'blood_group', 'location', 'contact_info')
+
+
+class AmbulanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'contact_info', 'district')
+
+
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(BloodDonor, BloodDonorAdmin)
+admin.site.register(Ambulance, AmbulanceAdmin)
